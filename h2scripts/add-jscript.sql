991e843e50dc16fdf7673f746d35f188b2065d8a
Темненков Кирилл Дмитриевич <ktemnenkov@intervale.ru>
2013-11-14 12:38:32 +0400

    useful scripts - fix error

--- h2scripts/add-jscript.sql
+++ h2scripts/add-jscript.sql
@@ -1,2 +1,3 @@
-INSERT INTO JSCRIPTS (ID, CONTENT) VALUES (1, 'helper.writeFileToEchoarea(""828.test"",""русский subject"",""/opt/jnode/rules/828.test.rules.txt"");');
+INSERT INTO SCRIPTHELPERS ( HELPER , CLASSNAME) VALUES ('writeFileToEchoareaHelper', 'jnode.jscript.WriteFileToEchoareaHelper');
+INSERT INTO JSCRIPTS (ID, CONTENT) VALUES (1, 'writeFileToEchoareaHelper.writeFileToEchoarea("828.test","русский subject","/opt/jnode/rules/828.test.rules.txt");');
 INSERT INTO SCHEDULES (DETAILS, JSCRIPT_ID, TYPE) VALUES (0, 1, 'DAILY');
