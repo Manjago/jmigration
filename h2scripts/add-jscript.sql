751aaea4eba830d3e71a54b36f998a7e01c7817e
Темненков Кирилл Дмитриевич <ktemnenkov@intervale.ru>
2013-11-14 13:20:27 +0400

    change table name

--- h2scripts/add-jscript.sql
+++ h2scripts/add-jscript.sql
@@ -1,3 +1,3 @@
 INSERT INTO SCRIPTHELPERS ( HELPER , CLASSNAME) VALUES ('writeFileToEchoareaHelper', 'jnode.jscript.WriteFileToEchoareaHelper');
 INSERT INTO JSCRIPTS (ID, CONTENT) VALUES (1, 'writeFileToEchoareaHelper.writeFileToEchoarea("828.test","русский subject","/opt/jnode/rules/828.test.rules.txt");');
-INSERT INTO SCHEDULES (DETAILS, JSCRIPT_ID, TYPE) VALUES (0, 1, 'DAILY');
+INSERT INTO SCHEDULE (DETAILS, JSCRIPT_ID, TYPE) VALUES (0, 1, 'DAILY');
